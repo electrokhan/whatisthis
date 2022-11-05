c1fb9f334deb09ef509afdfb4a999715aef06f09
electrokhan <mdnexter@gmail.com>
2022-11-05 01:15:25 -0300

    v2

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+let saveEl = document.getElementById("well")
+let countEl = document.getElementById("count")
 let count = 0
  
 
 function increment() {
-    count++
-    document.getElementById("count").innerText = count
+    count ++
+    countEl.textContent = count
 }
+
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+
+    countEl.textContent = 0
+    count = 0
+}
+
+
+
+
